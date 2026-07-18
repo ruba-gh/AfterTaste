94b7f53e2f5f9404b1edcddd17078bb4e15f213b
RaghadAljuid <s220003258@seu.edu.sa>
2026-07-18 10:39:48 +0300

    Add draft saving feature

--- AfterTaste/ContentView.swift
+++ AfterTaste/ContentView.swift
@@ -5,13 +5,21 @@
 //  Created by Ruba Alghamdi on 29/01/1448 AH.
 //
 
+//
+//  ContentView.swift
+//  AfterTaste
+//
+//  Created by Ruba Alghamdi on 29/01/1448 AH.
+//
+
 import SwiftUI
 
 struct ContentView: View {
     @StateObject private var purchaseVM = PurchaseViewModel()
     @StateObject private var draftStore = DraftStore()
-
+    
     @State private var selectedTab = 0
+    
 
     init() {
         let appearance = UITabBarAppearance()
